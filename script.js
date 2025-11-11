7cfde5fa0fe7d95644aa9e4f41f342a7ea8121ff
flogood <florescrystal36@gmail.com>
2025-11-10 20:00:16 -0800

    Added script.js and started practicing variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("JavaScript file loaded successfully!");
+// A variable for text (a "string")
+let firstName = "Alice"; 
+
+// A variable for a number
+let score = 95; 
+
+// A constant variable (it can't change later)
+const PI = 3.14;
+console.log("The user's name is: " + firstName);
+console.log("The score is: " + score);
